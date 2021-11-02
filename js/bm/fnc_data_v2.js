e24db5d59cc8f34c2f8c38cbaf70402af3829164
silly-programme <78978295+silly-programme@users.noreply.github.com>
2021-11-02 20:18:13 +0000

    Update fnc_data_v2.js
    Adding more members

--- js/bm/fnc_data_v2.js
+++ js/bm/fnc_data_v2.js
@@ -29,8 +29,8 @@ var int_Colspan = 2;
 var ary_TitleData = [
  "Core members"
  ,"Backup dancers"
- //,"Kamis"
- //,"Staff"
+ ,"Kamis"
+ ,"Staff"
 ];
 
 // * キャラクター情報（編集可能。最後の行に”,”を付けないようにしてください）
@@ -41,12 +41,24 @@ var ary_TitleData = [
 //                                      [1,2,3,4,5,6,7,8,9,
 var ary_CharacterData = [
   [1, "SU-METAL",                                                  [1,0,0,0,0,0,0,0], "bmppl/Su.png"],
-  [1, "KOBAMETAL",                                                 [1,0,0,0,0,0,0,0], "bmppl/Koba.jpg"],
+  [1, "YUIMETAL",                                                  [1,0,0,0,0,0,0,0], "bmppl/Yui.png"],
+  [1, "MOAMETAL",                                                  [1,0,0,0,0,0,0,0], "bmppl/Moa.jpg"],
  
-  [1, "YUIMETAL",                                                  [0,1,0,0,0,0,0,0], "bmppl/Yui.png"],
-  [1, "MOAMETAL",                                                  [0,1,0,0,0,0,0,0], "bmppl/Moa.jpg"],
   [1, "Riho",                                                      [0,1,0,0,0,0,0,0], "bmppl/Riho.png"],
   [1, "Momoko",                                                    [0,1,0,0,0,0,0,0], "bmppl/Momoko.png"],
-  [1, "Kano",                                                      [0,1,0,0,0,0,0,0], "bmppl/Kano.png"]
+  [1, "Kano",                                                      [0,1,0,0,0,0,0,0], "bmppl/Kano.png"],
+  [1, "Saya",                                                      [0,1,0,0,0,0,0,0], "bmppl/Saya.jpg"],
+ 
+  [1, "Mikio",                                                      [0,0,1,0,0,0,0,0], "bmppl/Mikio.png"],
+  [1, "Ohmura",                                                     [0,0,1,0,0,0,0,0], "bmppl/Ohmura.png"],
+  [1, "BOH",                                                        [0,0,1,0,0,0,0,0], "bmppl/BOH.png"],
+  [1, "Hideki",                                                     [0,0,1,0,0,0,0,0], "bmppl/Hideki.png"],
+  [1, "Leda",                                                       [0,0,1,0,0,0,0,0], "bmppl/Leda.png"],
+  [1, "ISAO",                                                       [0,0,1,0,0,0,0,0], "bmppl/ISAO.png"],
+  [1, "Yuya",                                                       [0,0,1,0,0,0,0,0], "bmppl/Yuya.png"],
+ 
+  [1, "KOBAMETAL",                                                 [0,0,0,1,0,0,0,0], "bmppl/Koba.jpg"],
+  [1, "Nora",                                                      [0,0,0,1,0,0,0,0], "bmppl/Nora.png"],
+  [1, "MIKIKOMETAL",                                               [0,0,0,1,0,0,0,0], "bmppl/Mikiko.png"]
   
 ];
